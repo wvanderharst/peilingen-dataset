50557c94f2c9e9346295ee13cd3c4daa147db294
wvanderharst <wouter.van.der.harst@gmail.com>
2024-10-03 21:10:12 +0200

    modified:   pages/2_ Voting_Power.py

--- pages/2_ Voting_Power.py
+++ pages/2_ Voting_Power.py
@@ -33,20 +33,36 @@ df3 = load_data2()
 partij = st.selectbox(
     "Partij",
     df3.Partij.unique())
-df = pd.read_csv(f'data_conditional/{partij}.csv')
+partij1 = partij.replace('/', '')
+
+df = pd.read_csv(f'data_conditional/{partij1}.csv')
 
 allparties = set(df3["Partij"])
 
 
 df['reger'] = df['reger'].apply(convert_string_to_list)
 
-st.write(all_in_list("VVD",partij))
 
-df= df[df['reger'].apply(lambda x: partij in x)]
+df34= df[df['reger'].apply(lambda x: partij in x)]
 
 st.dataframe(
-    df['reger'].value_counts()/lengthN)
+    df['reger'].value_counts()/len(df['reger']))
 
-df8 = (df['reger'].value_counts()/lengthN).sum()
+df8 = df['reger'].value_counts().sum()
+df34 = df34['reger'].value_counts().sum()
+#df8 = (df['reger'].value_counts()/len(df['reger'])).sum()
+#df34 = (df34['reger'].value_counts()/len(df34['reger'])).sum()
+num = df34/df8
+labels = 'Coaltitie', 'Oppositie'
+sizes = [num, 1-num ]
+explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-st.write(df8)
+fig1, ax1 = plt.subplots()
+
+
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+    shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title(partij)
+st.pyplot(fig1)
+
